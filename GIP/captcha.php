87703754b7168f3a5cfa9f4c2b57983430187e2e
Junior <nickje.debut@gmail.com>
2023-02-17 14:26:53 +0100

    Captcha
    Captcha image wont load

--- GIP/captcha.php
+++ GIP/captcha.php
@@ -1,29 +1,18 @@
-<?php
+﻿<?php
 session_start();
+header("Content-Type: image/png"); 
 $random_nummer = rand(10000, 99999); 
 
-$_SESSION["captcha"] = $random_number; 
+$_SESSION["captcha"] = $random_nummer; 
+
 
 $foto = imagecreate(100, 50); 
-$bg_color = imagecolorallocate($image, 255, 255, 255); 
-$text_color = imagecolorallocate($image, 0, 0, 0);
-imagestring($image, 5, 25, 18, $random_nummer, $text_color); 
+$bg_color = imagecolorallocate($foto, 255, 255, 255); 
+$text_color = imagecolorallocate($foto, 0, 0, 0);
+imagestring($foto, 5, 25, 18, $random_nummer, $text_color); 
 
 
-header("Content-Type: image/png"); 
-imagepng($image); 
-imagedestroy($image); 
+
+imagepng($foto); 
+imagedestroy($foto); 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Captcha</title>
-</head>
-<body>
-
-
-</body>
-</html>
