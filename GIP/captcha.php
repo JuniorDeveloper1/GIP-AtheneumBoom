c352ae991c103bf5a4e2b6ab783edc31018d5b80
Junior <nickje.debut@gmail.com>
2023-03-06 11:24:37 +0100

    Product ID
    When clicked on a product you're send to this page

--- GIP/captcha.php
+++ GIP/captcha.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 session_start();
 header("Content-Type: image/png"); 
 $random_nummer = rand(10000, 99999); 
@@ -9,7 +9,7 @@ $_SESSION["captcha"] = $random_nummer;
 $foto = imagecreate(100, 50); 
 $bg_color = imagecolorallocate($foto, 255, 255, 255); 
 $text_color = imagecolorallocate($foto, 0, 0, 0);
-imagestring($foto, 5, 25, 18, $random_nummer, $text_color); 
+  $captcha_text_color = imagecolorallocate($layer, 0, 0, 0);
 
 
 
