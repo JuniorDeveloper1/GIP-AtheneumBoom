d24b78abbffe337dd76fbcdde6a43dc709362efb
Junior <nickje.debut@gmail.com>
2023-04-17 11:25:52 +0200

    CSS

--- GIP/nl/login/loggedIn.php
+++ GIP/nl/login/loggedIn.php
@@ -5,22 +5,54 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+
+    <style> 
+    #login {
+        align-content: center;
+    }
+
+    #button {
+        background-color: rgb(38, 39, 43);
+        color: rgb(115, 115, 115);
+        height: 30px;
+        margin-top: 10px;
+        cursor: pointer;
+        width: 115px;
+        border-radius: 15px;
+    }
+
+    #button:hover {
+        border-color: rgb(38, 39, 43);
+    
+        background-color: white;
+        color: rgb(115, 115, 115);
+        height: 30px;
+        margin-top: 10px;
+        cursor: pointer;
+        width: 115px;
+        border-radius: 15px;
+    }
+
+    </style>
 </head>
 <body>
 
+
     <?php
      include '..\header.html'; 
-     echo "Je bent ingelogd!"
+   
      ?>
     <form name="form" method="POST">
+    <div id="login" align="middle">
         <?php
-        echo $_SESSION['klantID']."<br>";
-        echo $_SESSION['klantVoornaam']."<br>";
-        echo $_SESSION['klantGebruikersnaam']."<br>"  ;
-        echo $_SESSION['kantEmail']."<br>" ;
+          echo "Je bent ingelogd!"."<br>";
+        echo "KlantID: ".$_SESSION['klantID']."<br>";
+        echo "Voornaam: ".$_SESSION['klantVoornaam']."<br>";
+        echo "Gebruikersnaam: ".$_SESSION['klantGebruikersnaam']."<br>"  ;
+        echo "Email: ".$_SESSION['kantEmail']."<br>" ;
         ?>
         
-        <button name="logOut">Log OUT!</button>
+        <button name="logOut" id="button">LOG OUT</button>
         <?php 
         if(isset($_POST["logOut"])){
             $_SESSION["loggedIn"] = false;
@@ -30,8 +62,10 @@
             unset($_SESSION["klantGebruikersnaam"]);
             unset($_SESSION["kantEmail"]);
         }
-      
-
+        ?>
+    </div>
+   
+        <?php
         include '..\footer.html'; 
         ?>
     </form>
