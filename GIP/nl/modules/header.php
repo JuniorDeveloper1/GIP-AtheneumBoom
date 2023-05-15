5a8a8438e6fe02b0db1515b9b3a415124aef5857
Junior <nickje.debut@gmail.com>
2023-05-15 13:25:52 +0200

    Code clean up

--- GIP/nl/modules/header.php
+++ GIP/nl/modules/header.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>header</title>
 
-    <style>
+<style>
         ::-webkit-scrollbar {
            width: 10px;
         }
@@ -378,7 +378,7 @@
 
 
 
-    </style>
+</style>
 
     <script> 
       // var hamburger = document    .getElementById("hamburger_image");
